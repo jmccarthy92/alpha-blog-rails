0299cc68b40f48997e62e0737e643e78f4f2100b
jmccarthy92 <jmccarthyns92@gmail.com>
2018-07-17 16:41:48 +0000

    Update current_user to be article creator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,7 +22,7 @@ class ArticlesController < ApplicationController
     # to use byebug debugger
     # debugger 
     @article = Article.new(article_params)
-    # @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
