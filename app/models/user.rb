039d72495074c2523a53f9ed8221f952f2d6918d
jmccarthy92 <jmccarthyns92@gmail.com>
2018-07-17 17:04:39 +0000

    Add admin user functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@
 #  validating emails with regex
 
 class User < ActiveRecord::Base
-  has_many :articles
+  has_many :articles, dependent: :destroy
   before_save { self.email = email.downcase }
   validates :username, presence: true, 
     uniqueness: { case_sensitive: false }, 
